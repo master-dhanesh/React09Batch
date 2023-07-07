05a4481e859d288d3045dc346e22983406ce5cbc
master-dhanesh <dhanesh1296@gmail.com>
2023-07-07 18:25:32 +0530

    APP-01 Intro

--- app/page.js
+++ app/page.js
@@ -1,50 +1,15 @@
 "use client";
 import axios from "axios";
-import { useEffect, useState } from "react";
-
 const page = () => {
-    const [Images, setImages] = useState([]);
-    // const GetImages = () => {
-    //     axios
-    //         .get("https://picsum.photos/v2/list?page=2&limit=10")
-    //         .then((res) => console.log(res.data))
-    //         .catch((err) => console.log(err));
-    // };
+    const searchImages = () => {};
 
-    const GetImages = async () => {
-        try {
-            const { data } = await axios.get(
-                "https://picsum.photos/v2/list?page=2&limit=10"
-            );
-            console.log(data);
-            setImages(data);
-        } catch (error) {
-            console.log(error);
-        }
-    };
-
-    useEffect(() => {
-        GetImages();
-    }, []);
-
-    const renderImages = Images.map((image, i) => {
-        return (
-            <div key={image.id}>
-                <img height={200} src={image.download_url} alt="" />
-                <p>{image.author}</p>
-            </div>
-        );
-    });
-    return (
-        <div>
-            <h1>Homepage</h1>
-            {/* <button onClick={GetImages}>Get Images</button> */}
-            <hr />
-            {renderImages}
-        </div>
-    );
+    return <div>page</div>;
 };
 
 export default page;
 
-// https://picsum.photos/v2/list?page=1&limit=10
+// List of Images
+// https://api.unsplash.com/photos?client_id=ID&page=1
+
+// Seach Images
+// https://api.unsplash.com/search/photos?client_id=ID&page=1&query=office
