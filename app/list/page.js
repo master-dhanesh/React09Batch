38c4b4d6def1e4d1964a6e169030761ff8ff01fe
master-dhanesh <dhanesh1296@gmail.com>
2023-07-12 18:30:18 +0530

    search done

--- app/list/page.js
+++ app/list/page.js
@@ -4,8 +4,25 @@ import { DataContext } from "../context";
 
 const Trending = () => {
     const [data, setData] = useContext(DataContext);
-    console.log(data);
-    return <div>Trending</div>;
+
+    let listrender = <p>Loading...</p>;
+    if (data.length > 0) {
+        listrender = data.map((d, i) => {
+            return (
+                <li key={d.id}>
+                    <img src={d.links.download} alt="" />
+                    <h3>{d.user.first_name}</h3>
+                </li>
+            );
+        });
+    }
+
+    return (
+        <div className="list">
+            <h1>Trending Images</h1>
+            <ul>{listrender}</ul>
+        </div>
+    );
 };
 
 export default Trending;
