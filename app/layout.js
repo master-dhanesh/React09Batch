bd8c49d5e1ea4c7dea749dbe7fc0908e635f6d1b
master-dhanesh <dhanesh1296@gmail.com>
2023-08-16 18:31:01 +0530

    infinite scrolling

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,5 @@
 "use client";
 import "./globals.css";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import { Provider } from "react-redux";
-import { store } from "@/store/store";
 
 export const metadata = {
     title: "Create Next App",
@@ -13,12 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <body suppressHydrationWarning>
-                <Provider store={store}>
-                    {children}
-                    <ToastContainer />
-                </Provider>
-            </body>
+            <body suppressHydrationWarning>{children}</body>
         </html>
     );
 }
--- app/page.js
+++ app/page.js
@@ -1,61 +1,48 @@
 "use client";
-import Link from "next/link";
-import { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { asyncaddmovies } from "@/store/Actions/movieActions";
-import { changepage, removeerrors } from "@/store/Reducers/movieReducer";
-import { toast } from "react-toastify";
-
-export const metadata = {
-    title: "Tmdb Homepage",
-};
+import axios from "axios";
+import { useState, useEffect } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const page = () => {
-    const dispatch = useDispatch();
-    const { movies, page, errors } = useSelector((state) => state.movieReducer);
-    console.log(errors);
-
-    if (errors.length > 0) {
-        errors.map((e, i) => {
-            toast.error(e);
-        });
-        dispatch(removeerrors());
-    }
+    const [posts, setposts] = useState([]);
+    const [hasMore, setHasMore] = useState(true);
+    const getmoreposts = async () => {
+        try {
+            const { data } = await axios.get(
+                `https://jsonplaceholder.typicode.com/posts?_limit=10&_start=${posts.length}`
+            );
+            data.length === 0 ? setHasMore(false) : "";
+            setposts([...posts, ...data]);
+        } catch (err) {
+            console.log(err);
+        }
+    };
 
     useEffect(() => {
-        // if (movies.length === 0) dispatch(asyncaddmovies());
-        dispatch(asyncaddmovies());
-    }, [page]);
-
+        if (posts.length === 0) getmoreposts();
+    }, []);
     return (
         <div>
-            <h1>Navbar component</h1>
-            <h1>Search Header component</h1>
-            <hr />
-            <div className="d-flex container p-5 mt-3 flex-wrap">
-                {movies.map((m, i) => {
-                    return (
-                        <div className="me-3 mb-3" key={m.id}>
-                            <img
-                                height={200}
-                                src={`https://image.tmdb.org/t/p/w500${m.poster_path}`}
-                                alt=""
-                            />
-                            <p>
-                                <Link href={`/${m.id}`}>{m.title}</Link>
-                            </p>
-                        </div>
-                    );
-                })}
-            </div>
-            <hr />
-            <div className=" my-5 text-center paginate">
-                <button onClick={() => dispatch(changepage(-1))}>
-                    Previous
-                </button>
-                <span className="h1">{page}</span>
-                <button onClick={() => dispatch(changepage(1))}>Next</button>
-            </div>
+            <h1>Posts</h1>
+            <ul className="list-group">
+                <InfiniteScroll
+                    dataLength={posts.length}
+                    next={getmoreposts}
+                    hasMore={hasMore}
+                    loader={<h1>Loading...</h1>}
+                    endMessage={
+                        <p style={{ textAlign: "center" }}>
+                            <b>Yay! You have seen it all</b>
+                        </p>
+                    }
+                >
+                    {posts.map((p, idx) => (
+                        <li className="mb-5 alert alert-dark " key={idx}>
+                            {p.title}
+                        </li>
+                    ))}
+                </InfiniteScroll>
+            </ul>
         </div>
     );
 };
